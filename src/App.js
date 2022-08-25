1c94058dbd52f2c1e901b7fa84ba844f22f0d14d
localhead <dodoalive1994@gmail.com>
2022-08-25 22:44:17 +0300

    improved components logic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ Components in React - is just a JS functions
 import ExpenseItem from "./components/ExpenseItems";
 
 function App() {
+  // Data to paste into components
+  const data = [
+    {
+      id: "el1",
+      title: "Car",
+      date: new Date(2022, 2, 21),
+      amount: 34000,
+    },
+
+    {
+      id: "el2",
+      title: "iPhone 11",
+      date: new Date(2022, 2, 11),
+      amount: 520,
+    },
+
+    {
+      id: "el3",
+      title: "Laptop",
+      date: new Date(2022, 2, 23),
+      amount: 14200,
+    },
+  ];
+
   // this is what is will be rendered
   return (
     /* 
@@ -17,8 +41,23 @@ function App() {
         {" "}
         <h2>Let's get started!</h2>
         <p>The random text</p>
-        {/* We created our first component and just pasted it here like this. Cool */}
-        <ExpenseItem></ExpenseItem>
+        {/* We created our first component and just pasted it here like this. Cool 
+        Now we need to paste data inside of these components. Naming is crucial! */}
+        <ExpenseItem
+          title={data[0].title}
+          date={data[0].date}
+          amount={data[0].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={data[1].title}
+          date={data[1].date}
+          amount={data[1].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={data[2].title}
+          date={data[2].date}
+          amount={data[2].amount}
+        ></ExpenseItem>
       </div>
     </div>
   );
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,26 +1,32 @@
 // 1 - created component just by creating a function
-// All custom components must be in Capital letters in order to React to detect them
+// All custom components must be in Capital letters (ExpenseItem) in order to React to detect them
 /* 
 
 */
 // Adding CSS
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "iPhone 13 Pro";
-  const expenseAmount = 294.64;
-
+// props - is a parameter that hold all the values we recieve in component
+// name we recive from App.js must match!!!
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.toLocaleString("en-US", { year: "numeric" });
   return (
-    /* here we have className instead of just class */
+    /* here we have className instead of just class (like in html)*/
     <div className="expense-item">
       <div className="expense-item__description">
         {" "}
-        <div className="expense-item__date">{expenseDate.toDateString()}</div>
-        <h2 className="expense-item__title">{expenseTitle}</h2>
+        <div className="expense-item__date">
+          {" "}
+          <div>{day}</div>
+          <div>{month}</div>
+          <div>{year}</div>
+        </div>
+        <h2 className="expense-item__title">{props.title}</h2>
       </div>
 
-      <div className="expense-item__price">{expenseAmount}$</div>
+      <div className="expense-item__price">{props.amount}$</div>
     </div>
   );
 }
