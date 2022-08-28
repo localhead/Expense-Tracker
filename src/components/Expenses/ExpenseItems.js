6da7410e3e355e97f30d73439e56724c720e31d8
localhead <dodoalive1994@gmail.com>
2022-08-28 16:17:54 +0300

    event handler on btn

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -5,6 +5,7 @@
 */
 // Adding CSS
 import React from "react";
+import Card from "../UI/Card";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -12,17 +13,26 @@ import ExpenseDate from "./ExpenseDate";
 // props - is a parameter that hold all the values we recieve in component
 // name we recive from App.js must match!!!
 function ExpenseItem(props) {
+  const bthTitle = function () {
+    console.log("adadada");
+  };
+
   return (
     /* here we have className instead of just class (like in html)*/
-    <div className="expense-item">
-      <div className="expense-item__description">
+    <Card className="expense-item">
+      <div className="expense-item__left">
         {" "}
         {/* Making Date a different component */}
         <ExpenseDate date={props.date}></ExpenseDate>
         <h2 className="expense-item__title">{props.title}</h2>
       </div>
-      <div className="expense-item__price">{props.amount}$</div>
-    </div>
+      <div className="expense-item__rigth">
+        {" "}
+        <div className="expense-item__price">{props.amount}$</div>
+        {/* Adding Event lister on button*/}
+        <button onClick={bthTitle}>Change Title</button>
+      </div>
+    </Card>
   );
 }
 
